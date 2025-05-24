80df8d928a8db1ab8d3460682dcb66fee89f2e15
yanguanghui <3497809730@qq.com>
2025-05-24 20:19:05 +0800

    修改

--- SimpleDesign/WorkspaceProjectStudy/ygh_5_23/utils/src/lib.rs
+++ SimpleDesign/WorkspaceProjectStudy/ygh_5_23/utils/src/lib.rs
@@ -29,3 +29,24 @@ pub mod date {
         now.format("%Y-%m-%d").to_string()
     }
 }
+
+
+// 测试模块
+#[cfg(test)]
+mod tests {
+    use super::{id_gen, date};
+
+    #[test]
+    fn test_generate_id() {
+        let id = id_gen::generate_id();
+        assert_eq!(id.len(), 4);
+        assert!(id.chars().all(|c| c.is_ascii_alphanumeric()));
+    }
+
+    #[test]
+    fn test_format_date() {
+        let date = date::format_date();
+        assert_eq!(date.len(), 10);
+        assert!(date.contains('-'));
+    }
+}
